9bb713d7166c6b79242e7f73e1c66d9dd072a736
trent <tjacks58@hotmail.com>
2023-02-13 13:44:39 -0800

    Createcomputer choice function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,3 +1,17 @@
+/**
+ * Return a random choice (Rock, Paper or Scissors).
+ * 
+ * @returns {string} Computer's choice
+ */
 let getComputerChoice = () => {
-    
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    return choices[parseInt(Math.random(choices.length - 1))];
+}
+
+let playRound = (playerSelection, computerSelection) => {
+
+}
+
+let game = () => {
+
 }
